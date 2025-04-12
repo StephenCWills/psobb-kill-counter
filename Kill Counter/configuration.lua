8bde3064e30eaaa4649bbd72c0f7746c5658b886
Josh <165105701+Bennylavaa@users.noreply.github.com>
2025-04-11 20:43:27 -0400

    revamped settings

---
+++ Kill Counter/configuration.lua
@@ -0,0 +1,56 @@
+return {
+    configurationWindow = false,
+    globalCounterWindow = false,
+    globalCounterDetailWindow = false,
+    sessionCounterWindow = false,
+    sessionCounterDetailWindow = false,
+    sessionInfoWindow = true,
+    fontScale = 1.000000,
+
+    globalCounterDimensionsLocked = false,
+    globalCounterResizeLocked = false,
+    globalCounterMoveLocked = false,
+    globalCounterNoTitleBar = true,
+    globalCounterDifficulty = 3.000000,
+    globalCounterEpisode = 1.000000,
+    globalCounterSectionID = 1.000000,
+    globalCounterArea = 0.000000,
+
+    globalCounterDetailDimensionsLocked = false,
+    globalCounterDetailResizeLocked = false,
+    globalCounterDetailMoveLocked = false,
+    globalCounterDetailNoTitleBar = true,
+
+    sessionCounterDimensionsLocked = false,
+    sessionCounterResizeLocked = false,
+    sessionCounterMoveLocked = false,
+    sessionCounterNoTitleBar = true,
+    sessionCounterDifficulty = 3.000000,
+    sessionCounterEpisode = 1.000000,
+    sessionCounterSectionID = 1.000000,
+    sessionCounterArea = 0.000000,
+
+    sessionCounterDetailDimensionsLocked = false,
+    sessionCounterDetailResizeLocked = false,
+    sessionCounterDetailMoveLocked = false,
+    sessionCounterDetailNoTitleBar = true,
+
+    sessionInfoDimensionsLocked = false,
+    sessionInfoResizeLocked = false,
+    sessionInfoMoveLocked = false,
+    sessionInfoNoTitleBar = true,
+
+    globalCounterWindowDisplayMode = 3.000000,
+    globalCounterDetailWindowDisplayMode = 1.000000,
+    sessionCounterWindowDisplayMode = 3.000000,
+    sessionCounterDetailWindowDisplayMode = 1.000000,
+    sessionInfoWindowDisplayMode = 1.000000,
+
+    globalCounterWindowTransparent = false,
+    globalCounterDetailWindowTransparent = false,
+    sessionCounterWindowTransparent = false,
+    sessionCounterDetailWindowTransparent = false,
+    sessionInfoWindowTransparent = false,
+
+    lockRoomID = true
+}
